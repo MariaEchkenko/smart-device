4c5dac87bbdfc5adfa21e13c0f5a84511e39208c
Maria Echkenko <mashasly@mail.ru>
2021-08-08 17:39:35 +0300

    Реализует футер без декоратива

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -18,27 +18,3 @@ contactsButton.addEventListener('click', function () {
     navList.classList.remove('page-footer__list--active');
   }
 });
-
-'use strict';
-
-var popupButton = document.querySelector('.user-nav__button');
-var popup = document.querySelector('.popup');
-
-
-/*if (navMain) {
-  navMain.classList.remove('main-nav--nojs');
-}*/
-
-if (popupButton) {
-  popupButton.addEventListener('click', function () {
-    if (popup.classList.contains('popup--closed')) {
-      popup.classList.remove('popup--closed');
-      popup.classList.add('popup--opened');
-      document.body.classList.add('no-scroll');
-    } else {
-      popup.classList.add('popup--closed');
-      popup.classList.remove('popup--opened');
-      document.body.classList.remove('no-scroll');
-    }
-  });
-}

da59bf58722ba1780255b3fade6762a9cfe920f3
Maria Echkenko <mashasly@mail.ru>
2021-08-11 15:39:28 +0300

    Реализует js для попапа

--- build/js/script.js
+++ build/js/script.js
@@ -23,22 +23,65 @@ contactsButton.addEventListener('click', function () {
 
 var popupButton = document.querySelector('.user-nav__button');
 var popup = document.querySelector('.popup');
+var popupWrapper = document.querySelector('.popup__wrapper');
+var popupCloseButton = document.querySelector('.popup__button-close');
+var userName = document.querySelector('#popup-name');
+var userTel = document.querySelector('#popup-tel');
+var popupText = document.querySelector('#popup-text');
+
+let isStorageSupport = true;
+let storageName = '';
+let storageTel = '';
+let storageText = '';
+
+try {
+  storageName = localStorage.getItem('user-name');
+  storageTel = localStorage.getItem('user-tel');
+  storageText = localStorage.getItem('text');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 
-/*if (navMain) {
-  navMain.classList.remove('main-nav--nojs');
-}*/
+popupButton.addEventListener('click', function () {
+  popup.classList.add('popup--opened');
+  document.body.classList.add('no-scroll');
+  userName.focus();
 
-if (popupButton) {
-  popupButton.addEventListener('click', function () {
-    if (popup.classList.contains('popup--closed')) {
-      popup.classList.remove('popup--closed');
-      popup.classList.add('popup--opened');
-      document.body.classList.add('no-scroll');
-    } else {
-      popup.classList.add('popup--closed');
-      popup.classList.remove('popup--opened');
-      document.body.classList.remove('no-scroll');
+  if (storageName) {
+    userName.value = storageName;
+    userTel.value = storageTel;
+    popupText.value = storageText;
+  }
+
+  popupCloseButton.addEventListener('click', closePopup);
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      closePopupHandler();
     }
   });
+
+  popup.addEventListener('click', function (evt) {
+    if (evt.target !== popupWrapper) {
+      closePopupHandler();
+    }
+  });
+
+});
+
+popupWrapper.addEventListener('click', function (evt) {
+  evt.stopPropagation();
+});
+
+function closePopup() {
+  popup.classList.remove('popup--opened');
+  document.body.classList.remove('no-scroll');
+}
+
+function closePopupHandler() {
+  closePopup();
+  window.removeEventListener('keydown', closePopupHandler);
+  popupCloseButton.removeEventListener('click', closePopupHandler);
+  popupButton.removeEventListener('click', closePopupHandler);
 }
